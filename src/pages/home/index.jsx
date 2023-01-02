5b26865227bded821bc889eae3341dd12ba6c822
Guilherme Possebon <gpossebon67@gmail.com>
2023-01-02 14:22:50 -0300

    Organização de pastas

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+  <>
+    <h1>Lista de presença</h1>
+    <input type="text" placeholder="Digite um nome..."/>
+    <button type="button">Adicionar</button>
+  </>
+  )
+}
+
+export default Home
