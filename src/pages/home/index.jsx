70c64015bf8ad2d734d6d93c59d114a6fe050164
Guilherme Possebon <gpossebon67@gmail.com>
2023-01-02 16:11:17 -0300

    Exercício usando reactJs finalizado

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,17 +1,67 @@
 import './style.css'
-import {Card} from '../../components/Card'
-
+import {Card} from '../../components/Card/Index'
+import React, {useState, useEffect} from 'react'
 
 export function Home() {
-  return (
-    <div className="container">
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite um nome..."/>
-      <button type="button">Adicionar</button>
+  const [studentName, setStudentName] = useState('')
+  const [students, setStudents] = useState([])
+  const [user, setUser] = useState({name: '', avatar: ''})
 
-      <Card />
-      <Card />
-      <Card />
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleDateString('pt-br', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })  
+    }
+
+    setStudents(prevState => [...prevState, newStudent])
+  }
+
+
+  useEffect(() => {
+    // Corpo do useEffect
+    fetch('https://api.github.com/users/guilherme-possebon')
+    .then(response => response.json())
+    .then(data=> {
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      })
+    })
+  },[])
+
+
+  return (
+    <div className="container"> 
+
+      <header>
+        <h1>Lista de presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil" />
+        </div>
+      </header>
+
+      <input 
+      type="text" 
+      placeholder="Digite um nome..."
+      onChange={e => setStudentName(e.target.value)}
+      />
+
+      <button type="button" onClick={handleAddStudent}>Adicionar</button>
+
+      {
+        students.map(student => 
+          <Card 
+          key={student.time}
+          name={student.name} 
+          time={student.time} 
+        /> )
+      }
+
     </div>
   )
 }
